27b3d8abec44fb7e0180815af93c4847d846d518
Mauricio Antero <mjunior447@gmail.com>
2025-04-26 12:11:23 -0300

    feat: create service for qrcode generate; call qrcode generate service from the controller

--- src/main/java/com/mauricioantero/qrcode/generator/controller/QRCodeController.java
+++ src/main/java/com/mauricioantero/qrcode/generator/controller/QRCodeController.java
@@ -1,6 +1,8 @@
 package com.mauricioantero.qrcode.generator.controller;
 
+import com.mauricioantero.qrcode.generator.dto.QRCodeGenerateRequest;
 import com.mauricioantero.qrcode.generator.dto.QRCodeGenerateResponse;
+import com.mauricioantero.qrcode.generator.services.QRCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/qrcode")
 public class QRCodeController {
+
+    private final QRCodeGeneratorService qrCodeGeneratorService;
+
+    public QRCodeController(QRCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
-    public ResponseEntity<QRCodeGenerateResponse> generate() {
-        return null;
+    public ResponseEntity<QRCodeGenerateResponse> generate(@RequestBody QRCodeGenerateRequest request) {
+        try {
+            QRCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQRCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
